1b972730c8e5bdd1c0cd0bfb0fedaa15c82878a0
Justice7Axe <justice7axe@gmail.com>
2025-01-19 08:42:21 +0900

    1월19일 수정

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -15,6 +15,9 @@ const Image: React.FC<ImageProps> = ({ ariaHidden, src, alt, width, height }) =>
 };
 
 const Footer: React.FC = () => {
+
+  const searchHref = typeof window !== 'undefined' ? `${window.location.origin}/search` : '/search';
+  const searchSabreHref = typeof window !== 'undefined' ? `${window.location.origin}/search-sabre` : '/search-sabre';
     return (
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
@@ -34,7 +37,9 @@ const Footer: React.FC = () => {
           </a>
           <a
             className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://select-search-zeta.vercel.app/search"
+            // href="https://select-search-zeta.vercel.app/search"
+            // href={`${window.location.origin}/search`}
+            href={searchHref}
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -49,7 +54,9 @@ const Footer: React.FC = () => {
           </a>
           <a
             className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://select-search-zeta.vercel.app/search-sabre"
+            // href="https://select-search-zeta.vercel.app/search-sabre"
+            // href={`${window.location.origin}/search-sabre`}
+            href={searchSabreHref}
             target="_blank"
             rel="noopener noreferrer"
           >
