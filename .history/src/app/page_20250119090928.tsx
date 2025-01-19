fbd1c3ea13575622bd570f95998418e40ab33b18
Justice7Axe <justice7axe@gmail.com>
2025-01-19 09:10:25 +0900

    랜딩 수정정

--- .history/src/app/page_20250119090928.tsx
+++ .history/src/app/page_20250119090928.tsx
@@ -3,7 +3,7 @@ import Footer from "./components/footer";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main></main>
       <Footer />
     </div>
